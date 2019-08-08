d6caf103551cdf782617131092a1ca613e71f0dd
Jayson Potter <jpotter@topiatechnology.com>
2019-08-08 15:34:05 -0700

    Added Network Issue Component and route

--- src/router.js
+++ src/router.js
@@ -6,57 +6,75 @@ import EventShow from './views/EventShow.vue'
 import NProgress from 'nprogress'
 import store from '@/store/store'
 import NotFound from "./views/NotFound";
+import NetworkIssue from "./views/NetworkIssue";
 
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'event-list',
-      component: EventList,
-      props: true
-    },
-    {
-      path: '/event/create',
-      name: 'event-create',
-      component: EventCreate
-    },
-    {
-      path: '/event/:id',
-      name: 'event-show',
-      component: EventShow,
-      props: true,
-      beforeEnter(routeTo, routeFrom, next) {
-        store.dispatch('event/fetchEvent', routeTo.params.id)
-            .then(event => {
-              routeTo.params.event = event
-              next()
-            })
-            .catch(() => next({name: '404', params: { resource: 'event'}}))
-      }
-    },
-    {
-      path: '/404',
-      name: '404',
-      component: NotFound,
-      props: true
-    },
-    {
-      path: '*',
-      redirect: { name: '404', params: {resource: 'page'}}
-    }
+    mode  : 'history',
+    routes: [
+        {
+            path     : '/',
+            name     : 'event-list',
+            component: EventList,
+            props    : true
+        },
+        {
+            path     : '/event/create',
+            name     : 'event-create',
+            component: EventCreate
+        },
+        {
+            path     : '/event/:id',
+            name     : 'event-show',
+            component: EventShow,
+            props    : true,
+            beforeEnter(routeTo, routeFrom, next) {
+                store.dispatch('event/fetchEvent', routeTo.params.id)
+                    .then(event => {
+                        routeTo.params.event = event
+                        next()
+                    })
+                    .catch(error => {
+                      if(error.response && error.response.status === 404) {
+                        next({name: '404', params: {resource: 'event'}})
+                      } else {
+                        next({name: 'network-issue'})
+                      }
+
+                    })
+            }
+        },
+        {
+            path     : '/404',
+            name     : '404',
+            component: NotFound,
+            props    : true
+        },
+        {
+            path     : '/network-issue',
+            name     : 'network-issue',
+            component: NetworkIssue
+        },
+        {
+            path    : '*',
+            redirect: {
+                name  : '404',
+                params: {
+                    resource: 'page'
+                }
+            }
+        }
     ]
 })
 
 router.beforeEach((routeTo, routeFrom, next) => {
-  NProgress.start()
-  next()
+    NProgress.start()
+    next()
 })
 
 router.afterEach(() => {
-  NProgress.done();
+    NProgress.done();
 })
 
 export default router
