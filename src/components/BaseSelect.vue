537e5531a50b81810b4220dfb3ea10a4ead1b7e6
Jayson Potter <jpotter@topiatechnology.com>
2019-08-13 16:52:21 -0700

    Base select needed a little update
    instead of @input on the select it needed to be @change

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
         <label v-if="label">{{ label }}</label>
-        <select type="text" :value="value" @input="updateValue" v-bind="$attrs" v-on="$listeners">
-            <option v-for="option in options" :key="option" :selected="option === value">{{ option }}</option>
+        <select type="text" :value="value" @change="updateValue" v-bind="$attrs" v-on="$listeners">
+            <option
+                    v-for="option in options"
+                    :value="option"
+                    :key="option"
+                    :selected="option === value">{{ option }}</option>
         </select>
     </div>
 </template>
